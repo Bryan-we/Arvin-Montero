f4ddd5adab8d48246f9a84ef02d5c914c0c9b1cf
Bryan Peñafiel <110010415+Bryan-we@users.noreply.github.com>
2023-06-05 20:16:53 -0500

    Add files via upload

---
+++ js/aumentodeimg.js
@@ -0,0 +1,10 @@
+const FulImgBox = document.getElementById("fulImgBox"),
+  fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference) {
+  FulImgBox.style.display = "flex";
+  fulImg.src = reference;
+}
+function closeImg() {
+  FulImgBox.style.display = "none";
+}
